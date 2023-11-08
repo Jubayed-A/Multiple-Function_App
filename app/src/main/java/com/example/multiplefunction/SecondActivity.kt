6b1e66536ee1c38abd1bead8e5741b1e8f3dcc70
Jubayed-A <learnigprograming@gmail.com>
2023-11-09 04:05:31 +0600

    Some code add

--- app/src/main/java/com/example/multiplefunction/SecondActivity.kt
+++ app/src/main/java/com/example/multiplefunction/SecondActivity.kt
@@ -1,11 +1,32 @@
 package com.example.multiplefunction
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.cardview.widget.CardView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val androidActivity = findViewById<CardView>(R.id.android)
+        val iosActivity = findViewById<CardView>(R.id.ios)
+        val flutterActivity = findViewById<CardView>(R.id.flutter)
+        val reactActivity = findViewById<CardView>(R.id.react)
+        val webActivity = findViewById<CardView>(R.id.web)
+        val machineActivity = findViewById<CardView>(R.id.machine)
+        val button = findViewById<Button>(R.id.butContact)
+
+        // Button code here
+
+        button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel: " + "01303564615")
+            startActivity(intent)
+        }
+
     }
 }
