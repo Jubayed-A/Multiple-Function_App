b0520fa3003389db5483c266f72019b3e87206ec
Jubayed-A <learnigprograming@gmail.com>
2023-11-09 03:50:51 +0600

    Some file add and some file update

---
+++ app/src/main/java/com/example/multiplefunction/AndoridActivity.kt
@@ -0,0 +1,11 @@
+package com.example.multiplefunction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AndoridActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_andorid)
+    }
+}
